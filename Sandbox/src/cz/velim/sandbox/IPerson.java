304741a0b7b570f0241afda731b0b81c19ca716e
Michal Velek <mvelek@gmail.com>
2013-08-02 14:32:00 +0200

    add method getName() to interface IPerson

--- Sandbox/src/cz/velim/sandbox/IPerson.java
+++ Sandbox/src/cz/velim/sandbox/IPerson.java
@@ -3,5 +3,6 @@ package cz.velim.sandbox;
 public interface IPerson {
 	
 	Integer getSalary();
+	String getName();
 
 }
--- Sandbox/src/cz/velim/sandbox/Sandbox.java
+++ Sandbox/src/cz/velim/sandbox/Sandbox.java
@@ -4,7 +4,23 @@ public class Sandbox {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		IPerson person = new IPerson() {
+			
+			@Override
+			public Integer getSalary() {
+				return 0;
+			}
 
+			@Override
+			public String getName() {
+				return "mike";
+			}
+			
+		};
+		
+		System.out.println(person.getSalary());
+		System.out.println(person.getName());
 	}
 
 }
