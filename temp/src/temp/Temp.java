06709fa7866cd563bbcb326eb5cbada02a02883f
Michal Velek <mvelek@gmail.com>
2013-10-16 11:09:05 +0200

    temporary save

--- temp/src/temp/Temp.java
+++ temp/src/temp/Temp.java
@@ -1,6 +1,8 @@
 package temp;
 
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.HashMap;
@@ -41,11 +43,10 @@ public class Temp extends Foo {
 		// for (Integer key : ks)
 		// System.out.println(hm.get(key));
 
-		// List<String> list = Arrays.asList("a", "x", "a", "a", "a", "a", "x",
-		// "a", "a");
-		// for (String s : list) {
-		// System.out.println(s);
-		// }
+		List<String> list = Arrays.asList("a", "x", "a", "a", "a", "a", "x", "a", "a");
+		for (String s : list) {
+			System.out.println(s);
+		}
 
 		TreeMap<Integer, String> tm = new TreeMap<Integer, String>();
 
@@ -61,14 +62,17 @@ public class Temp extends Foo {
 		}
 
 		// treemap test
-		Map<Integer, Person> hmap = new TreeMap<Integer, Person>( // sorting treemap using comparator
+		Map<Integer, Person> hmap = new TreeMap<Integer, Person>( // sorting
+																	// treemap
+																	// using
+																	// comparator
 				new Comparator<Integer>() {
 					@Override
 					public int compare(Integer o1, Integer o2) {
 						return o1 - o2;
 					}
 				});
-		for (int i = 0; i < 50000; i++)
+		for (int i = 0; i < 50; i++)
 			hmap.put(i, new Person(String.valueOf(i), UUID.randomUUID()
 					.toString()));
 
@@ -78,7 +82,7 @@ public class Temp extends Foo {
 
 			@Override
 			public int compare(Person o1, Person o2) {
-				//return o1.getUUID().compareTo(o2.getUUID());
+				// return o1.getUUID().compareTo(o2.getUUID());
 				String s1 = o1.getUUID().split("-")[1];
 				String s2 = o2.getUUID().split("-")[1];
 				return s1.compareTo(s2);
@@ -86,34 +90,50 @@ public class Temp extends Foo {
 
 		});
 
-		// sorted arraylist - sorted by UUID by Comparator 
+		// sorted arraylist - sorted by UUID by Comparator
 		for (Person p : lp) {
-			//System.out.println(p.getName());
-			System.out.println(p.getUUID());
+			// System.out.println(p.getName());
+			//System.out.println(p.getUUID());
 		}
 
 		System.out.println(" * * * * * * *");
 
-		// iterate via keys
-		for (Integer key : hmap.keySet()) {
-			System.out.println(hmap.get(key).getName());
-			System.out.println(hmap.get(key).getUUID());
-		}
+//		// iterate via keys
+//		for (Integer key : hmap.keySet()) {
+//			System.out.println(key.hashCode());
+//			System.out.println(hmap.get(key).getName());
+//			System.out.println(hmap.get(key).getUUID());
+//		}
 
 		System.out.println("\n * * * * * * *");
 
 		// iterate using iterator from entrySet
-		Iterator<Entry<Integer, Person>> hmit = hmap.entrySet().iterator();
-		while (hmit.hasNext()) {
-			Person pers = hmit.next().getValue();
-			System.out.println(pers.getName());
-			System.out.println(pers.getUUID());
+		// Iterator<Entry<Integer, Person>> hmit = hmap.entrySet().iterator();
+		// while (hmit.hasNext()) {
+		// Person pers = hmit.next().getValue();
+		// System.out.println(pers.getName());
+		// System.out.println(pers.getUUID());
+		// }
+
+		System.out.println(test());
+
+	}
+
+	@SuppressWarnings("finally")
+	private static int test() {
+		try {
+			throw new IOException();
+		} catch (Exception e) {
+			System.out.println("catch");
+			return 1;
+		} finally {
+			return 2;
 		}
 
 	}
-	
-//	static void test(Map map){
-//		map.
-//	}
+
+	// static void test(Map map){
+	// map.
+	// }
 
 }
